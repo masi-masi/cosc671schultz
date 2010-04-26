1df0013440a12c074b35090b0a4c7d6ba7f5764d
tara.schultz@gmail.com <tara.schultz@gmail.com@53c0f778-15c1-11df-9d17-5fea08ab653e>
2010-04-26 17:11:28 +0000

    Adding files for Tasks 4 and 5

--- trunk/VendingMachine/src/VendingMachineItemTest.java
+++ trunk/VendingMachine/src/VendingMachineItemTest.java
@@ -27,24 +27,91 @@ public class VendingMachineItemTest extends TestCase {
 	 * Test method for {@link VendingMachineItem#VendingMachineItem(java.lang.String, double)}.
 	 */
 	public void testVendingMachineItem() {
-		// initialize item with null 
-		item = null;
+		/* Test: if item is uninitialized item is null 
+		 * EO: item is null
+		 */
 		assertNull(item);
 		
-		// initialize item with string and double calling constructor 
+		/* Test: Use the constructor to create new VendingMachineItems and verify the 
+		 *       name and price of the new VendingMachineItems are correct.
+		 * EO: "Coca-Cola" is new VendingMachineItem name
+		 *     $1.25 is new VendingMachineItem price
+		 */
+		assertEquals("Coca-Cola", new VendingMachineItem("Coca-Cola", 1.25).getName());
+		assertEquals(1.25, new VendingMachineItem("Coca-Cola", 1.25).getPrice());
+		
+		/* Test: Initialize item with name ("Coca-Cola") and price (1.25) with constructor
+		 *       and calling getName() and getPrice() after item is initialized .
+		 * EO: item not null
+		 *     item name = "Coca-Cola"
+		 *     item price = $1.25
+		 */
 		item = new VendingMachineItem("Coca-Cola", 1.25);
 		assertNotNull(item);
-		// item name is set correctly
-		assertEquals("Coca-Cola", new VendingMachineItem("Coca-Cola", 1.25).getName());
-		// item price is set correctly
-		assertEquals(1.25, new VendingMachineItem("Coca-Cola", 1.25).getPrice());
+		assertEquals("Coca-Cola", item.getName());
+		assertEquals(1.25, item.getPrice());
+		
+		/* Test: Initialize item with name ("Coca-Cola") and price (0.00) with constructor
+		 *       calling getPrice to verify correct price after item is initialized.
+		 * EO: item is not null
+		 *     item price = $0.00
+		 */
+		// initialize item with string and double = 0
+		item = new VendingMachineItem("Coca-Cola", 0.00);
+		assertNotNull(item);
+		assertEquals(0.00, item.getPrice());
+		
+		/* Test: Initialize item with name ("Coca-Cola") and price (-.50) with constructor
+		 *       calling getPrice to verify correct price after item is initialized
+		 * EO: Throws VendingMachineException
+		 */
+		/* verify if item is null, a NullPointerException is thrown */
+		try {
+			item = new VendingMachineItem("Coca-Cola", -0.50);
+		} catch (VendingMachineException npe) {
+			return;
+		}
+		fail("Expected VendingMachineException");
+		
+		/* Test: Initialize item with name (" ") and price (1.00) with contstructor
+		 *       calling getName to verify correct name after item is initialized
+		 * EO: item is not null
+		 *     item name is " "
+		 */
+		item = new VendingMachineItem(" ", 1.00);
+		assertNotNull(item);
+		assertEquals(" ", item.getName());
+		
+		/* Test: Initialize item with name ("") and price (1.00) with constructor 
+		 *       calling getName to verify correct name after item is initialized
+		 * EO: item is not null
+		 *     item name is ""
+		 */
+		item = new VendingMachineItem("", 1.00);
+		assertNotNull(item);
+		assertEquals("", item.getName());
+		
+		/* Test: Initialize item with name (null) and price (1.00) with constructor
+		 *       calling getName to verify correct name after item is initialized
+		 * EO: item is not null
+		 *     item name is null
+		 */
+		item = new VendingMachineItem(null, 1.00);
+		assertNotNull(item);
+		assertNull(item.getName());
 	}
 
 	/**
 	 * Test method for {@link VendingMachineItem#getName()}.
 	 */
 	public void testGetName() {
+		/* Test: Verify that if item is null a NullPointerException is thrown
+		 *       when getName() is called
+		 * EO: item is null
+		 *     Throw NullPointerException
+		 */
 		/* verify if item is null, a NullPointerException is thrown */
+		assertNull(item);
 		try {
 			assertNull(item.getName());
 		} catch (NullPointerException npe) {
@@ -52,16 +119,53 @@ public class VendingMachineItemTest extends TestCase {
 		}
 		fail("Expected NullPointerException");
 		
-		/* initialize item using constructor then call getName() */
+		/* Test: Verify that if item name is set to "Coca-Cola" with constructor
+		 *       item name is "Coca-Cola"
+		 * EO: item is not null
+		 *     item name is "Coca-Cola"
+		 */
 		item =  new VendingMachineItem("Coca-Cola", 1.25);
+		assertNotNull(item);
 		assertEquals("Coca-Cola", item.getName());
+		
+		/* Test: Verify that if item name is set to " " with constructor 
+		 *       item name is " "
+		 * EO: item is not null
+		 *     item name is " "
+		 */
+		item = new VendingMachineItem(" ", 1.00);
+		assertNotNull(item);
+		assertEquals(" ", item.getName());
+		
+		/* Test: Verify that if item name is set to "" with constructor
+		 *       item name is ""
+		 * EO: item is not null
+		 *     item name is ""
+		 */
+		item = new VendingMachineItem("", 1.00);
+		assertNotNull(item);
+		assertEquals("", item.getName());
+		
+		/* Test: Verify that if item name is set to null with constructor
+		 *       item name is null
+		 * EO: item is not null
+		 *     item name is null
+		 */
+		item = new VendingMachineItem(null, 1.00);
+		assertNotNull(item);
+		assertNull(item.getName());
 	}
 
 	/**
 	 * Test method for {@link VendingMachineItem#getPrice()}.
 	 */
 	public void testGetPrice() {
-		/* verify if item is null, a NullPointerException is thrown */
+		/* Test: Verify that if item is null a NullPointerException is thrown
+		 *       when getPrice() is called.
+		 * EO: item is null
+		 *     Throw NullPointerException
+		 */
+		assertNull(item);
 		try {
 			assertNull(item.getPrice());
 		} catch (NullPointerException npe) {
@@ -69,9 +173,34 @@ public class VendingMachineItemTest extends TestCase {
 		}
 		fail("Expected NullPointerException");
 		
-		/* initialize item using constructor then call getPrice() */
+		/* Test: Verify that if item price is set to 1.25 with constructor
+		 *       item price is 1.25
+		 * EO: item is not null
+		 *     item price is $1.25
+		 */
 		item = new VendingMachineItem("Coca-Cola", 1.25);
+		assertNotNull(item);
 		assertEquals(1.25, item.getPrice());
+		
+		/* Test: Verify that if item price is set to 0.00 with constructor
+		 *       item price is $0.00
+		 * EO: item is not null
+		 *     item price = $0.00
+		 */
+		item = new VendingMachineItem("Coca-Cola", 0.00);
+		assertNotNull(item);
+		assertEquals(0.00, item.getPrice());
+		
+		/* Test: Verify that if item price is set to -0.50 with constructor
+		 *       a VendingMachineException is thrown.
+		 * EO: Throws VendingMachineException
+		 */
+		try {
+			item = new VendingMachineItem("Coca-Cola", -0.50);
+		} catch (VendingMachineException npe) {
+			return;
+		}
+		fail("Expected VendingMachineException");
 	}
 
 }
--- trunk/VendingMachine/src/VendingMachineTest.java
+++ trunk/VendingMachine/src/VendingMachineTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 /**
@@ -5,16 +7,22 @@ import junit.framework.TestCase;
  */
 
 /**
- * @author tarayang
+ * @author Tara Schultz
  *
  */
 public class VendingMachineTest extends TestCase {
+	
+	VendingMachine machine;
+	VendingMachineItem item;
 
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp();
+		
+		machine = new VendingMachine();
+		item = new VendingMachineItem("Coca-Cola", 1.25);
 	}
 
 	/* (non-Javadoc)
@@ -22,48 +30,277 @@ public class VendingMachineTest extends TestCase {
 	 */
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		
+		machine = null;
+		item = null;
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#addItem(VendingMachineItem, java.lang.String)}.
 	 */
 	public void testAddItem() {
-		fail("Not yet implemented");
+		/* Test: Slot code A is empty, add item to slot A
+		 * EO: Item is at slot code A
+		 */
+		assertEquals(null, machine.getItem(VendingMachine.A_CODE));
+		machine.addItem(item, VendingMachine.A_CODE);
+		assertSame(item, machine.getItem(VendingMachine.A_CODE));
+		
+		/* Test: Slot code B is empty, add null item to slot B
+		 * EO: Null is at slot code B (B is empty)
+		 */
+		machine.addItem(null, VendingMachine.B_CODE);
+		assertNull(machine.getItem(VendingMachine.B_CODE));
+		
+		/* Test: Slot code A is occupied, add item to slot A
+		 * EO: VendingMachineException thrown
+		 */
+		try {
+			machine.addItem(new VendingMachineItem("Pepsi", 1.00), VendingMachine.A_CODE);
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Slot A already occupied");
+		
+		/* Test: Slot code F is not valid.
+		 * EO: VendingMahcineException thrown
+		 */
+		try {
+			machine.addItem(item, "F");
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Slot F is invalid");
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#removeItem(java.lang.String)}.
 	 */
 	public void testRemoveItem() {
-		fail("Not yet implemented");
+		/*
+		 * Test: Remove item from empty slot B
+		 * EO: VendingMachineException thrown
+		 */
+		assertNull(machine.getItem(VendingMachine.B_CODE));
+		try {
+			machine.removeItem(VendingMachine.B_CODE);
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Slot B is empty");
+		
+		/*
+		 * Test: Remove item from invalid slot F
+		 * EO: VendingMachineException thrown
+		 */
+		try {
+			machine.removeItem("F");
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Slot F is invalid");
+		
+		/*
+		 * Test: Remove item from valid occupied slot A
+		 * EO: item removed from slot A (slot A empty)
+		 */
+		machine.addItem(item, VendingMachine.A_CODE);
+		assertNotNull(machine.getItem(VendingMachine.A_CODE));
+		machine.removeItem(VendingMachine.A_CODE);
+		assertNull(machine.getItem(VendingMachine.A_CODE));
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#insertMoney(double)}.
 	 */
 	public void testInsertMoney() {
-		fail("Not yet implemented");
+		/*
+		 * Test: Insert money with amount less than zero
+		 * EO: VendingMachineException thrown
+		 */
+		try {
+			machine.insertMoney(-.50);
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Amount less than 0");
+		
+		/*
+		 * Test: Insert money with amount 0.00, initial balance is 0
+		 * EO: balance is 0.00
+		 */
+		assertEquals(0.00, machine.getBalance());
+		machine.insertMoney(0.00);
+		assertEquals(0.00, machine.getBalance());
+		
+		/*
+		 * Test: Insert money with amount 5.75, initial balance is 0
+		 * EO: balance is 5.75
+		 */
+		assertEquals(0.00, machine.getBalance());
+		machine.insertMoney(5.75);
+		assertEquals(5.75, machine.getBalance());
+		
+		/*
+		 * Test: Insert money with amount 0.00, initial balance is 5.75
+		 * EO: balance is 5.75
+		 */
+		assertEquals(5.75, machine.getBalance());
+		machine.insertMoney(0.00);
+		assertEquals(5.75, machine.getBalance());
+		
+		/*
+		 * Test: Insert money with amount 0.00, initial balance is -.4.00
+		 * EO: VendingMachineException thrown
+		 */
+		machine.balance = -4.00;
+		machine.insertMoney(0.00);
+		try {
+			machine.getBalance();
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Balance is less than zero");
+		
+		/*
+		 * Test: Insert money with amount 1.00, initial balance is 5.75
+		 * EO: balance is 6.75
+		 */
+		machine.balance = 5.75;
+		assertEquals(5.75, machine.getBalance());
+		machine.insertMoney(1.00);
+		assertEquals(6.75, machine.getBalance());
+
+		/*
+		 * Test: Insert money with amount 1.00, initial balance is -3.00
+		 * EO: VendingMachineException thrown (balance should not be less than 0)
+		 */		
+		machine.balance = -3.00;
+		try {
+			machine.insertMoney(1.00);
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Balance is less than zero");
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#getBalance()}.
 	 */
 	public void testGetBalance() {
-		fail("Not yet implemented");
+		/*
+		 * Test: Vending machine balance starts with 0.00
+		 * EO: balance is 0.00
+		 */
+		assertEquals(0.00, machine.getBalance());
+		
+		/*
+		 * Test: Get balance when balance is 5.00 (greater than 0)
+		 * EO: balance is 5.00
+		 */
+		machine.balance = 5.00;
+		assertEquals(5.00, machine.balance);
+		assertEquals(machine.balance, machine.getBalance());
+		
+		/*
+		 * Test: Get balance when balance is 0.00
+		 * EO: balance is 0.00
+		 */
+		machine.balance = 0.00;
+		assertEquals(0.00, machine.balance);
+		assertEquals(machine.balance, machine.getBalance());
+		
+		/*
+		 * Test: Get balance when balance is -3.25 (less than 0)
+		 * EO: VendingMachineException thrown (balance should not be less than 0)
+		 */
+		machine.balance = -3.25;
+		try {
+			double balance = machine.getBalance();
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Balance is less than 0");
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#makePurchase(java.lang.String)}.
 	 */
 	public void testMakePurchase() {
-		fail("Not yet implemented");
+		/*
+		 * Test: Make purchase from occupied slot A, balance is 2.00
+		 * EO: return true
+		 *     balance is .75
+		 *     item at slot A is null
+		 */
+		machine.addItem(item, VendingMachine.A_CODE);
+		machine.balance = 2.00;
+		assertTrue(machine.makePurchase(VendingMachine.A_CODE));
+		assertNull(machine.getItem(VendingMachine.A_CODE));
+		assertEquals(.75, machine.getBalance());
+		
+		/*
+		 * Test: Make purchase from unoccupied slot A, balance is 2.00
+		 * EO: return false
+		 *     balance is 2.00
+		 *     item at slot A is null
+		 */
+		machine.balance = 2.00;
+		assertFalse(machine.makePurchase(VendingMachine.A_CODE));
+		assertNull(machine.getItem(VendingMachine.A_CODE));
+		assertEquals(2.00, machine.getBalance());
+		
+		
+		/*
+		 * Test: Make purchase from occupied slot B, balance is 1.00
+		 * EO: return false
+		 *     balance is 1.00
+		 *     item at slot B is null
+		 */
+		machine.balance = 1.00;
+		machine.addItem(item, VendingMachine.B_CODE);
+		assertFalse(machine.makePurchase(VendingMachine.B_CODE));
+		
+		/*
+		 * Test: Make purchase from unoccupied slot, balance is 1.00
+		 * EO: return false
+		 *     balance is 1.00
+		 */
 	}
 
 	/**
 	 * Test method for {@link VendingMachine#returnChange()}.
 	 */
 	public void testReturnChange() {
-		fail("Not yet implemented");
+		/*
+		 * Test: Return change when purchasing 1.25 item with balance of 2.50
+		 * EO: return change 1.25
+		 */
+		machine.balance = 2.50;
+		machine.addItem(item, VendingMachine.A_CODE);
+		machine.makePurchase(VendingMachine.A_CODE);
+		assertEquals(1.25, machine.returnChange());
+		
+		/*
+		 * Test: Return change when purchasing 1.25 item with balance of 1.00
+		 * EO: return change 1.00
+		 */
+		machine.balance = 1.00;
+		machine.addItem(item, VendingMachine.A_CODE);
+		machine.makePurchase(VendingMachine.A_CODE);
+		assertEquals(1.00, machine.returnChange());
+		
+		/*
+		 * Test: Return change when purchasing 1.25 item with balance of -2.00
+		 * EO: throw VendingMachineException can't return balance change of -2.00
+		 */
+		machine.balance = -2.00;
+		try {
+			machine.makePurchase(VendingMachine.A_CODE);
+		} catch (VendingMachineException vme) {
+			return;
+		}
+		fail("Expected VendingMachineException - Cannot return negative change");
+		
 	}
-
 }
